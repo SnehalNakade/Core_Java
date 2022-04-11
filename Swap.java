e5e7e29527aa88d86b736de37cb0db477b989e88
SnehalNakade <snehalnakade98@gmail.com>
2022-04-11 09:04:20 +0530

    Added: Swap.java

---
+++ Swap.java
@@ -0,0 +1,20 @@
+package AssignmentDay5;
+
+import java.util.Scanner;
+
+public class Swap {
+	public static void main(String[] args) {
+		
+		  int first, second, temp; // x and y are to swap   
+	       Scanner sc = new Scanner(System.in);  
+	       System.out.println("Enter the value of first and second number :");  
+	      first = sc.nextInt();  
+	       second = sc.nextInt();  
+	       System.out.println("Before swapping: "+first +"  "+ second);  
+	       temp = first;  
+	       first = second;  
+	       second = temp;  
+	       System.out.println("After swapping: "+first +"   " + second);  
+	       System.out.println( );  
+	    }    
+}
